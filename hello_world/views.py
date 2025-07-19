7e5d9cc985b1d24a707a1486e281e5cee8462516
liambaker93 <3c2mrjqh@students.codeinstitute.net>
2025-07-19 20:23:13 +0100

    adjust index function in hello_world/views.py to check for the request method and display a result to the user

--- hello_world/views.py
+++ hello_world/views.py
@@ -4,5 +4,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+    if request == POST:
+        return HttpResponse("You must have POSTed something")
+    else:
+        return HttpResponse(request.method)
 
