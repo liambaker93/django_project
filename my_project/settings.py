8f508abb122b8b686c0e558f6cd1434e95d5092a
liambaker93 <3c2mrjqh@students.codeinstitute.net>
2025-07-19 21:23:53 +0100

    change debug value back to false

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nf_2-iq#m=opr6%wktnf1)m)=#f26s^^3&llt83n^+vfovis67'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
+DEBUG = False 
 
 ALLOWED_HOSTS = [
     '.herokuapp.com',
